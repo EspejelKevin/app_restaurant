1035f152c4f166bb53e97e71844181dcc24a0d36
EspejelKevin <kevin.espejelmtz@gmail.com>
2021-11-05 09:49:15 -0600

    App Restaurant

--- src/db.py
+++ src/db.py
@@ -1,6 +1,6 @@
 import pymysql
 
-mysql = pymysql.connect(host='localhost', 
+mysql = pymysql.connect(host='127.0.0.1', 
                         port=3306, 
                         user='root', 
                         passwd='km05010002', 
